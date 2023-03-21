94337625476ce032a641c07e29883e57e2635076
adelin04 <adelin.marin04@yahoo.com>
2023-03-21 20:07:28 +0200

    first commit

---
+++ src/components/_Utils/SliderUsers.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//style
+import "./SliderUsers.css";
+
+const SliderUsers = ({ isActiv, usersList }) => {
+  return (
+    <div className={`slider-users ${isActiv}`}>
+      {usersList}
+    </div>
+  );
+};
+
+export default SliderUsers;
